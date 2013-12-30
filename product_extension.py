2c5dd38a1c0a8afa31866b7c941ae132a2f90090
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-12-30 11:05:42 +0200

    corrected module name

--- product_extension.py
+++ product_extension.py
@@ -4,7 +4,8 @@ class product_extension(osv.osv):
 	_name='product.product'
 	_inherit='product.product'
 	_columns= {
-		'x_quality_check': fields.text('Quality check text'),
+		'customs_code': fields.text('Customs code'),
+        'country_of_origin': fields.many2one('res.country','Country of Origin')
 	}
 
 product_extension()
