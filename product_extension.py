b1a59ae688f2eac79a87f48d50adfdecc377b24c
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-12-30 11:07:25 +0200

    implementing fields

--- product_extension.py
+++ product_extension.py
@@ -4,7 +4,7 @@ class product_extension(osv.osv):
 	_name='product.product'
 	_inherit='product.product'
 	_columns= {
-		'customs_code': fields.text('Customs code'),
+		'customs_code': fields.char('Customs code'),
         'country_of_origin': fields.many2one('res.country','Country of Origin')
 	}
 
