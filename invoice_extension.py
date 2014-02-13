81e2bf0f2e134a77738ffa4cd369b4e58a2d728d
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2014-02-13 12:58:42 +0200

    changed module name. added finnish translations

--- invoice_extension.py
+++ invoice_extension.py
@@ -6,8 +6,8 @@ class invoice_extension_intrastat(osv.osv):
 	_inherit='account.invoice'
     
 	_columns= {
-		'transaction_type': fields.selection((('0', _('Procurement/Sale')), ('1', _('Sample')), ('2', _('Leasing')), ('3', _('Other')), ('4', _('Return')), ('5', _('Replacement')), ('6', _('Repair'))), 'Transaction Type', required=False),
-        'transportation_method': fields.selection((('0', _('Sea')), ('1', _('Train')), ('2', _('Road')), ('3', _('Air')), ('4', _('Mail')), ('5', _('Static')), ('6', _('Freshwater')), ('7', _('Self transporting'))), 'Transportation Method', required=False)
+		'transaction_type': fields.selection((('11', _('Procurement/Sale')), ('12', _('Sample')), ('14', _('Leasing')), ('19', _('Other')), ('21', _('Return')), ('22', _('Replacement')), ('60', _('Repair'))), 'Transaction Type', required=False),
+        'transportation_method': fields.selection((('1', _('Sea')), ('2', _('Train')), ('3', _('Road')), ('4', _('Air')), ('5', _('Mail')), ('7', _('Static')), ('8', _('Freshwater')), ('9', _('Self transporting'))), 'Transportation Method', required=False)
 	}
 
 invoice_extension_intrastat()
